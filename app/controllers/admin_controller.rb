45dc858f54a8d3fca4719fa7a124a5d071d50be4
Gilad Manor <gilad@quizic.co>
2013-11-15 02:50:13 +0200

    admin layout

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,45 @@
 class AdminController < ApplicationController
+  before_filter :auth_filter, :except=>[:login]
 
 
+  def login
+    if auth(params[:login], params[:password])
+      redirect_to action:"index" 
+      return 
+    end
+    logger.debug "XXX"
+    render "login", :layout=>false
+  end
+  
+  
 
   def index
+  
   end
+  
+  private
+  
+  def auth(login, password)
+    logger.info "logging in #{login}"
+    user = User.find_by_login_and_password(login, password)
+    if user
+      
+      session[:user_id] = user.id 
+      true
+    else
+      false
+    end
+  end
+  
+  def auth_filter
+    logger.debug "="*40
+    @user = User.find(session[:user_id]) unless session[:user_id].nil? 
+    unless @user
+      @error="Please Login"
+      render "login", :layout=>false
+      return false
+    end
+    @account = @user.account
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Ulixes::Application.routes.draw do
   
-  get 'api(/:action(/:id))(.:format)' , :controller=>:api
-  get 'admin(/:action)(.:format)' , :controller=>:admin
+  get 'api(/:action(/:id))' , :controller=>:api
   
-  get '(/:action(/:entity(/:id)))(.:format)', :controller=>:lsd
+  post 'admin(/:action)' , :controller=>:admin
+  get 'admin(/:action)' , :controller=>:admin
+  
+  post '(/:action(/:entity(/:id)))', :controller=>:lsd
   
   
 end
