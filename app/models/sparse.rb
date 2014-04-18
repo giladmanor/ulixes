c97b51996fbd99036a0ca68941c3e06c6b6a0ac0
Gilad Manor <giladmanor01@gmail.com>
2014-04-18 17:08:33 +0300

    Sparse GMM peek finder stable

--- app/models/sparse.rb
+++ app/models/sparse.rb
@@ -11,25 +11,34 @@ class Sparse
   def vote(paragons)
     paragon_pull = paragons.map{|p| p}
     point_count =Array.new(paragons.length,1)
+    mover_index = []
     @points.shuffle.each{|p|
     #Calculate pull factors for each paragon
       pull_factors = paragons.map{|pr| distance(p,pr)}
-      mover = paragons[pull_factors.index(pull_factors.min)]
-      p "#{p} Moves #{mover} to #{add(paragon_pull[paragons.index(mover)],p,2)} "
-      paragon_pull[paragons.index(mover)] = add(paragon_pull[paragons.index(mover)],p,2)#,@points.length)
-      point_count[paragons.index(mover)] +=1
+      
+      mover_index = pull_factors.index(pull_factors.min)
+      mover = paragons[mover_index]
+      
+      #p "#{p} Moves #{mover} to #{add(paragon_pull[paragons.index(mover)],p,2)} "
+      
+      paragon_pull[mover_index] = add(paragon_pull[mover_index],p)#,@points.length)
+      point_count[mover_index] +=1
     }
     #paragons.map.with_index{|p,i| sub(paragon_pull[i],p,2)}
     p point_count
-    paragon_pull.map.with_index{|p,i| add(p,p,2)}
-  end
-  
-  def sub(p1,p2,f=1)#returns a new point/vector
-    p1.map.with_index{|c,i| (c - p2[i]).to_f/f}
+    paragon_pull.map.with_index{|p,i| div(p,point_count[i])}
   end
   
   def add(p1,p2,f=1)
     p1.map.with_index{|c,i| (c+p2[i]).to_f/f}
+  end
+  
+  def div(p,f)
+    p.map{|c| c.to_f/f}
+  end
+  
+  def sub(p1,p2,f=1)#returns a new point/vector
+    p1.map.with_index{|c,i| (c - p2[i]).to_f/f}
   end
   
   def move(p1,p2,f)
