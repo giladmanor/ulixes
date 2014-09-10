fe2b8d177b60fd01bcfd7051be612d53bc9e88d9
Gilad <giladmanor01@gmail.com>
2014-09-10 12:34:31 +0300

    development rolling log file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,8 @@ Ulixes::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+  
+  config.logger = Logger.new(Rails.root.join("log",Rails.env + ".log"),3,5*1024*1024)
+  
 end
